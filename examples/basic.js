06a4d75637fe50f7b6258320acd61e6a05e31f16
James Speirs <james.speirs@gmail.com>
2016-11-29 10:30:44 -0700

    update example

--- examples/basic.js
+++ examples/basic.js
@@ -1,53 +1,45 @@
-var schema = require('../index');
-
-var personSchema;
-var result;
+'use strict';
+const Schema        = require('../bin/schema');
 
 //define the person schema
-personSchema = schema({
+const schema = Schema({
     age: {
-        defaultValue: 0,
+        default: 0,
         help: 'This must be a non-negative number.',
         transform: function(value) {
             return Math.round(value);
         },
-        validate: function(value, is) {
-            return !is.nan(value) && value >= 0;
+        validate: function(value) {
+            return !isNaN(value) && value >= 0;
         }
     },
     name: {
         help: 'This must be a non-empty string.',
         required: true,
-        validate: function(value, is) {
-            return is.string(value) && value;
+        validate: function(value) {
+            return typeof value === 'string' && value && true;
         }
     }
 });
 
-//define a function that uses the person schema to normalize input configurations
-function personData(person) {
-    var config = personSchema.normalize(person);
-    return 'Name: ' + config.name + ', Age: ' + config.age;
+// age transformation
+const ageTransform = schema.normalize({ name: 'Bob', age: 12.5 });
+console.log(ageTransform);      // Name: Bob, Age: 13
+
+// default age
+const defaultAge = schema.normalize({ name: 'Bob' });
+console.log(defaultAge);        // Name: Bob, Age: 0
+
+// invalid configuration - name is required
+try {
+    schema.normalize({})
+} catch (e) {
+    console.log(e.message);    // Missing required configuration property: name. This must be a non-empty string.
 }
 
-//call personData with a configuration
-result = personData({ name: 'Bob', age: 12.5 });     // Name: Bob, Age: 13
-console.log(result);
-
-//call personData with a configuration
-result = personData({ name: 'Bob' });                // Name: Bob, Age: 0
-console.log(result);
-
-//call personData with an invalid configuration
+// invalid configuration - age less than zero
 try {
-    personData({});
+    schema.normalize({ name: 'Bob', age: -5 });
 } catch (e) {
-    console.log(e.message);                         // Missing required configuration property: name. This must be a non-empty string.
-}
-
-//call personData with an invalid configuration
-try {
-    personData({ name: 'Bob', age: -5 });
-} catch (e) {
-    console.log(e.message);                         // Invalid configuration value for property: age. This must be a non-negative number. Received: -5
+    console.log(e.message);    // Invalid configuration value for property: age. This must be a non-negative number. Received: -5
 }
